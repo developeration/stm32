c250887e3a993c6f23010b2578e6b27f60bc48c5
jiangping.ma <oncwnuNwMZWxrj7AkMfKHUz1ToKA@git.weixin.qq.com>
2022-04-26 14:58:57 +0800

    ~

--- Server/Listener.ps1
+++ Server/Listener.ps1
@@ -26,8 +26,64 @@ while($true){
         $stream.close()
         }
 
+$sql = @"
+DECLARE @content nvarchar(500) = '$reqstr'
+
+INSERT INTO [dbo].[GPSLog]  ([content])  VALUES (@content)
+"@
+
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = "Server=$server;Database=$database;Integrated Security=SSPI;"
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.CommandText = $sql
+    $SqlCmd.Connection = $SqlConnection
+    $SqlConnection.open()
+    $SqlCmd.ExecuteNonQuery()
+    $SqlConnection.Close()
+
+
+
     try{
-    #$data = "+CIPGSMLOC: 0,40.033862,116.263082,2000/01/01,04:53:41"
+       #$reqstr = "+CIPGSMLOC: 0,40.033862,116.263082,2000/01/01,04:53:41"
+        if($reqstr.IndexOf("GPRMC") -gt 0 ){
+            $datasplit = $reqstr.Split(",") 
+            if(($datasplit.Length -eq 13) -and ($datasplit[2] -eq "A")){
+                $latitude = $datasplit[3]
+                $longitude = $datasplit[5]
+
+                Write-Host $latitude "~" $longitude
+       
+$sql = @"
+DECLARE @latitude nvarchar(50) = '$latitude'
+DECLARE @longitude nvarchar(50) = '$longitude'
+
+
+INSERT INTO [dbo].[GPSRecord]
+([latitude],[longitude],[recordtype])
+VALUES
+( @latitude,@longitude,1 )
+"@
+
+                $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+                $SqlConnection.ConnectionString = "Server=$server;Database=$database;Integrated Security=SSPI;"
+                $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+                $SqlCmd.CommandText = $sql
+                $SqlCmd.Connection = $SqlConnection
+                $SqlConnection.open()
+                $SqlCmd.ExecuteNonQuery()
+                $SqlConnection.Close()
+            }
+        }
+    }catch
+    {
+        Write-Host $_
+    }
+
+
+
+
+    try{
+    #$data = "$GPRMC,062616.00,A,4002.05679,N,11615.70555,E,0.462,,260422,,,A*7E"
         if($reqstr.IndexOf("CIPGSMLOC:") -gt 0 ){
             $datasplit = $reqstr.Substring(11,$reqstr.Length-11).Split(",")
             if($datasplit.Length -eq 5){
@@ -42,10 +98,9 @@ DECLARE @longitude nvarchar(50) = '$longitude'
 
 
 INSERT INTO [dbo].[GPSRecord]
-([latitude]
-,[longitude])
+([latitude],[longitude],[recordtype])
 VALUES
-( @latitude,@longitude )
+( @latitude,@longitude ,2)
 "@
 
                 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
