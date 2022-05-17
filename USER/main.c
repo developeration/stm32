3e7f30bc6d6c0e513306b7eed0b38b191cc8b6af
jiangping.ma <oncwnuNwMZWxrj7AkMfKHUz1ToKA@git.weixin.qq.com>
2022-05-17 11:08:05 +0800

    ~

--- USER/main.c
+++ USER/main.c
@@ -73,12 +73,10 @@ int main(void)
 		if(res){ FlashLedFail(4); Delay_Ms(1000);continue; }else {FlashLedOK(1);}
 		res = SIM900A_CONNECT_SERVER_SEND_INFOR((u8*)"wix.eastus.cloudapp.azure.com",(u8*)"9000");	
 		if(res){ FlashLedFail(8); Delay_Ms(1000);continue; }else {FlashLedOK(1);} 
-		res = SIM900A_GET_LOCATION();
-		if(res){ FlashLedFail(4); Delay_Ms(1000);continue; }else {FlashLedOK(1);}
 		SIM900A_GPRS_SEND_DATA(SIM_Location); 
 		FlashLedOK(1);
 		CAT4GPOWEROFF();
-		for(oo = 0;oo < 8 ; oo++){ 
+		for(oo = 0;oo < 12 ; oo++){ 
 			SystemSleep();
 			FlashLedOK(1);
 		}
